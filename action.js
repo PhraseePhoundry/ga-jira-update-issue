9c4cf555988a5b97bf380e9b46d9a0bdc89548e0
Emily Birch <emily.birch@phrasee.co>
2022-09-16 14:49:23 +0100

    initial commit of fetching jira issue

---
+++ action.js
@@ -0,0 +1,24 @@
+const Jira = require('./common/net/Jira')
+
+module.exports = class {
+  constructor ({ githubEvent, argv, config }) {
+    this.Jira = new Jira({
+      baseUrl: config.baseUrl,
+      token: config.token,
+      email: config.email,
+    })
+
+    this.config = config
+    this.argv = argv
+    this.githubEvent = githubEvent
+  }
+
+  async execute () {
+    const issueId = this.argv.issue || this.config.issue || null
+
+    console.log(`Fetching issue ${issueId}`)
+    console.log(await this.Jira.getIssue(issueId))
+
+    return {}
+  }
+}
